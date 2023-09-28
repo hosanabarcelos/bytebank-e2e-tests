6ca14e27084c83b65abf2c9e8873aca34e7001ea
Hosana <hosanabarceloscandido@gmail.com>
2023-09-28 16:18:26 -0300

    test: add caso de teste para aceso de página de cartões

---
+++ cypress/e2e/pages.cy.js
@@ -0,0 +1,12 @@
+describe('Testing multiple pages', () => {
+    it('if it is possible to access the cards page', () => {
+        cy.visit('/');
+        cy.getByData('botao-login').click();
+        cy.getByData('email-input').type('alien@email.com');
+        cy.getByData('senha-input').type('123456');
+        cy.getByData('botao-enviar').click();
+
+        cy.getByData('app-home').find('a').eq(1).click();
+        cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões');
+    });
+});
