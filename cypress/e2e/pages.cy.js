1ac79998c1c43cdb1ca6fc9c468a454eab188f8b
Hosana <hosanabarceloscandido@gmail.com>
2023-09-28 16:31:35 -0300

    test: usa location para verificar página atual

--- cypress/e2e/pages.cy.js
+++ cypress/e2e/pages.cy.js
@@ -6,7 +6,11 @@ describe('Testing multiple pages', () => {
         cy.getByData('senha-input').type('123456');
         cy.getByData('botao-enviar').click();
 
+        cy.location('pathname').should('eq', '/home');
+
         cy.getByData('app-home').find('a').eq(1).click();
         cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões');
+
+        cy.location('pathname').should('eq', '/home/cartoes')
     });
 });
